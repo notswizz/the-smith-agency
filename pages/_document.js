c951f5afd548b9f8bd00d8363fb174fb6b2bc2f1
jack smith <emailswizz@gmail.com>
2025-07-24 13:53:54 -0500

    okok

--- pages/_document.js
+++ pages/_document.js
@@ -42,9 +42,29 @@ export default function Document() {
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         
+        {/* Additional iOS icon meta tags for better compatibility */}
+        <meta name="apple-touch-fullscreen" content="yes" />
+        <meta name="apple-mobile-web-app-orientations" content="portrait" />
+        
+        {/* Preconnect for better icon loading */}
+        <link rel="preconnect" href="/" />
+        
         {/* Favicon and Icons */}
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        
+        {/* Apple Touch Icons - Multiple sizes for better iOS compatibility */}
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png?v=2" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png?v=2" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/apple-touch-icon-167x167.png?v=2" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/apple-touch-icon-152x152.png?v=2" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon-120x120.png?v=2" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/icons/apple-touch-icon-114x114.png?v=2" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icons/apple-touch-icon-76x76.png?v=2" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-60x60.png?v=2" />
+        <link rel="apple-touch-icon" sizes="40x40" href="/icons/apple-touch-icon-40x40.png?v=2" />
+        <link rel="apple-touch-icon" sizes="29x29" href="/icons/apple-touch-icon-29x29.png?v=2" />
+        
+        {/* Standard Icons */}
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
         <link rel="shortcut icon" href="/favicon.png" />
